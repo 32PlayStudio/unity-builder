a385c423b5d3495c48bc1128052501c9193f3cde
Daniel Bischoff <dnlbschff@users.noreply.github.com>
2022-07-21 23:36:56 +0200

    add software-properties-common

--- dist/platforms/ubuntu/steps/install-blender.sh
+++ dist/platforms/ubuntu/steps/install-blender.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 echo "Installing Blender"
+apt-get update
+apt-get install software-properties-common -y
 add-apt-repository ppa:savoury1/blender
-apt update
-apt install blender -y
+apt-get update
+apt-get install blender -y
 
 echo "Installed Blender version:"
 blender --version
