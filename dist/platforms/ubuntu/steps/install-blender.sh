2e51c755412447792a611ca85c31eebbbebdc690
Daniel Bischoff <dnlbschff@users.noreply.github.com>
2022-07-21 23:55:15 +0200

    add depending PPAs

--- dist/platforms/ubuntu/steps/install-blender.sh
+++ dist/platforms/ubuntu/steps/install-blender.sh
@@ -3,8 +3,12 @@
 echo "Installing Blender"
 apt-get update
 apt-get install software-properties-common -y
+add-apt-repository ppa:savoury1/gcc-defaults-9
+add-apt-repository ppa:savoury1/ffmpeg4
+add-apt-repository ppa:savoury1/display
 add-apt-repository ppa:savoury1/blender
 apt-get update
+apt-get upgrade -y && apt-get dist-upgrade -y
 apt-get install blender -y
 
 echo "Installed Blender version:"
